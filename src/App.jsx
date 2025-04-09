c41545f47352cca19b2f60ff1d7c258139934be6
KemonoPL1 <166020478+KemonoPL1@users.noreply.github.com>
2025-04-09 14:39:46 +0200

    dzis 9.11

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,39 @@
 import './App.css'
 
 function App() {
+  let a=3, b=1;
+  let c=a+b;
+  console.log('to');
+  console.log(c);
+  const komp=1;
+  let w1,w2,w3,w4,w5,w6,w7,w8,w9,w10;
+  w1=komp*1;
+  w2=komp*2;
+  w3=komp*3;
+  w4=komp*4;
+  w5=komp*5;
+  w6=komp*6;
+  w7=komp*7;
+  w8=komp*8;
+  w9=komp*9;
+  w10=komp*10;
+
   
 
   return (
     <>
-     
+     <div>
+     <p>{komp}*1={w1}</p>
+     <p>{komp}*2={w2}</p>
+      <p>{komp}*3={w3}</p>
+      <p>{komp}*4={w4}</p>
+      <p>{komp}*5={w5}</p>
+      <p>{komp}*6={w6}</p>
+      <p>{komp}*7={w7}</p>
+      <p>{komp}*8={w8}</p>
+      <p>{komp}*9={w9}</p>
+      <p>{komp}*10={w10}</p>
+     </div>
     </>
   )
 }
